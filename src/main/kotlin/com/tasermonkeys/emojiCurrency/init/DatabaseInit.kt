25b1f312cfd14e42761726eed920be1b82645a6d
James Stapleton <github@tasermonkeys.com>
2017-06-25 12:32:59 -0400

    again

--- src/main/kotlin/com/tasermonkeys/emojiCurrency/init/DatabaseInit.kt
+++ src/main/kotlin/com/tasermonkeys/emojiCurrency/init/DatabaseInit.kt
@@ -5,6 +5,8 @@ import com.tasermonkeys.emojiCurrency.models.EmojiLedger
 import com.tasermonkeys.emojiCurrency.models.Users
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.ThreadLocalTransactionManager
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
@@ -22,8 +24,8 @@ class PrepareTables {
         val local = "cedar-14" != System.getenv("STACK")
         val dbUrl: DatabaseUrl = DatabaseUrl.extract(local);
         val driverName = dbUrl.connection.metaData.driverName;
-        println("${dbUrl} -- ${driverName} -- ${DATABASE_URL}")
-        Database.connect(dbUrl.jdbcUrl(), org.postgresql.Driver::class.java.canonicalName)
+
+        Database.connect(DATABASE_URL, org.postgresql.Driver::class.java.canonicalName)
         transaction {
             SchemaUtils.create(Users, EmojiLedger)
         }
