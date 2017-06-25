d5784f9b2ce265d19615bff5707c2658ace74b61
James Stapleton <github@tasermonkeys.com>
2017-06-25 12:22:52 -0400

    again

--- src/main/kotlin/com/tasermonkeys/emojiCurrency/init/DatabaseInit.kt
+++ src/main/kotlin/com/tasermonkeys/emojiCurrency/init/DatabaseInit.kt
@@ -17,10 +17,12 @@ class PrepareTables {
 
     @PostConstruct
     fun init() {
+        var DATABASE_URL = System.getenv("DATABASE_URL")
         DriverManager.registerDriver(org.postgresql.Driver())
         val local = "cedar-14" != System.getenv("STACK")
         val dbUrl: DatabaseUrl = DatabaseUrl.extract(local);
         val driverName = dbUrl.connection.metaData.driverName;
+        println("${dbUrl} -- ${driverName} -- ${DATABASE_URL}")
         Database.connect(dbUrl.jdbcUrl(), org.postgresql.Driver::class.java.canonicalName)
         transaction {
             SchemaUtils.create(Users, EmojiLedger)
