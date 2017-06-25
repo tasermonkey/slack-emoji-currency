ffcbe2a7dade43d53c07177874c09a65aea00734
James Stapleton <github@tasermonkeys.com>
2017-06-25 11:53:12 -0400

    database problems

--- src/main/kotlin/com/tasermonkeys/emojiCurrency/init/DatabaseInit.kt
+++ src/main/kotlin/com/tasermonkeys/emojiCurrency/init/DatabaseInit.kt
@@ -14,17 +14,10 @@ class PrepareTables {
 
     @PostConstruct
     fun init() {
-        val dev = System.getenv("DATABASE_URL") == null
-        if (dev) {
-            println("************* Running with dev's H2 database *************")
-            Class.forName("org.h2.Driver")
-            Database.connect("jdbc:h2:./application.db", driver = "org.h2.Driver")
-        } else {
-            val local = "cedar-14" != System.getenv("STACK")
-            val dbUrl: DatabaseUrl = DatabaseUrl.extract(local);
-            val driverName = dbUrl.connection.metaData.driverName;
-            Database.connect(dbUrl.jdbcUrl(), driverName)
-        }
+        val local = "cedar-14" != System.getenv("STACK")
+        val dbUrl: DatabaseUrl = DatabaseUrl.extract(local);
+        val driverName = dbUrl.connection.metaData.driverName;
+        Database.connect(dbUrl.jdbcUrl(), driverName)
         transaction {
             SchemaUtils.create(Users, EmojiLedger)
         }
